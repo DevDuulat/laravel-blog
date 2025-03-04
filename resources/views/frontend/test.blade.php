2d30ca11672552d2ef2e0e0454f8039921cf67eb
DevDuulat <duulat.bekm@mail.ru>
2025-03-04 14:48:39 +0300

    Add frontend routes and pages for layout

---
+++ resources/views/frontend/test.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto p-4">
+        <h1 class="text-3xl font-bold text-center mb-4">Страница для вёрстки</h1>
+    </div>
+@endsection
