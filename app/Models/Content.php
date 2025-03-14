741d73f12630e8b08cb8dca9002f0dc1e3fed4ff
DevDuulat <duulat.bekm@mail.ru>
2025-03-14 12:08:44 +0300

    Create comment migration and model

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -39,4 +39,9 @@ class Content extends Model
         return $this->belongsTo(Category::class);
     }
 
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
 }
