d7cb9a4413be848f865d2cb478c51e53155a3a50
DevDuulat <duulat.bekm@mail.ru>
2025-04-17 01:58:24 +0600

    fix migrations

--- database/migrations/2025_03_15_170239_create_tests_table.php
+++ database/migrations/2025_03_15_170239_create_tests_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('tests', function (Blueprint $table) {
             $table->id();
-
-            $table->unsignedBigInteger('category_id');
+            $table->uuid('category_id');
 
             $table->json('title');
             $table->integer('duration');
@@ -24,6 +23,7 @@ return new class extends Migration
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
+
 
 
     /**
