617db257e9a631cf76349385689c4efbbe2bae92
DevDuulat <duulat.bekm@mail.ru>
2026-01-12 23:51:43 +0600

    fix(core): Remove redundant Alpine.js imports
    Livewire 3 automatically handles Alpine.js, making manual imports in app.js unnecessary and potentially causing conflicts. This change ensures proper integration and avoids duplication.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 
+// Удалите или закомментируйте эти строки,
+// так как Livewire 3 сам импортирует и запускает Alpine.
+/*
 import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
-
 Alpine.start();
+*/
